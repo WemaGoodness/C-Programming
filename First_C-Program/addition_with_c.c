e2a2eacd3d27fecd047802d9e26d2773ded1725d
WemaGoodness <goodnesswema20@gmail.com>
2023-09-30 18:49:01 +0000

    Learning C

---
+++ First_C-Program/addition_with_c.c
@@ -0,0 +1,17 @@
+/*This function adds two intigers and returns the result*/
+#include <stdio.h>
+int intiger_add( int x, int y)
+{
+	int result;
+	result = x + y;
+	return result;
+}
+
+int main()
+{
+	int sum;
+
+	sum = intiger_add(5,12);
+	printf("The addition of 5 and 12 is %d.\n", sum);
+	return 0;
+}
