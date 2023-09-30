6aa133871eeafc42d5e4e20a7fa7c49a25b0b296
WemaGoodness <goodnesswema20@gmail.com>
2023-09-30 19:21:55 +0000

    Learning C

---
+++ First_C-Program/printing_characters_on_screen.c
@@ -0,0 +1,14 @@
+/*Printing characters on screen*/
+#include <stdio.h>
+
+main()
+{
+	char c1;
+	char c2;
+
+	c1 = 'A';
+	c2 = 'a';
+	printf("Convert the value of c1 to character: %c.\n", c1);
+	printf("Convert the value of c2 to character: %c.\n", c2);
+	return 0;
+}
